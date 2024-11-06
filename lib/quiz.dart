dd15c849b1c7f2b4cff05cafaedfa666ac40ee5f
ALUNO <ALUNO@senaiescola.fsa>
2024-11-06 08:47:02 -0300

    Alteração de tudo

---
+++ lib/quiz.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+class PaginaQuiz extends StatefulWidget {
+  @override
+  _PaginaQuizState createState() => _PaginaQuizState();
+}
+
+class _PaginaQuizState extends State<PaginaQuiz> {
+  int indicePergunta = 0;
+  Map<String, int> pontuacoes = {}; // Armazenar pontuações por opção
+
+  List<Map<String, dynamic>> perguntas = [
+    {
+      'asset': 'lib/assets/gatito.jpg',
+      'texto': 'O que é Flutter?',
+      'opcoes': {
+        'Uma linguagem de programação': 0,
+        'Um framework para desenvolvimento de aplicativos': 1,
+        'Um banco de dados': 0,
+        'Um sistema operacional': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito2.jpg',
+      'texto':
+          'Qual linguagem de programação é usada para desenvolver aplicativos Flutter?',
+      'opcoes': {
+        'Java': 0,
+        'Dart': 1,
+        'Kotlin': 0,
+        'JavaScript': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito3.jpg',
+      'texto': 'Qual widget é usado como contêiner básico em Flutter?',
+      'opcoes': {
+        'Column': 0,
+        'Row': 0,
+        'Container': 1,
+        'Scaffold': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito4.jpg',
+      'texto': 'Qual comando é usado para iniciar um novo projeto Flutter?',
+      'opcoes': {
+        'flutter create nome_do_projeto': 1,
+        'flutter new nome_do_projeto': 0,
+        'flutter start nome_do_projeto': 0,
+        'flutter init nome_do_projeto': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito5.jpg',
+      'texto': 'Qual widget é usado para exibir uma lista de itens em Flutter?',
+      'opcoes': {
+        'Column': 0,
+        'ListView': 1,
+        'GridView': 0,
+        'Stack': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito6.jpg',
+      'texto':
+          'Qual comando é usado para compilar um aplicativo Flutter para a web?',
+      'opcoes': {
+        'flutter build web': 1,
+        'flutter web build': 0,
+        'flutter create web': 0,
+        'flutter deploy web': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito7.jpg',
+      'texto': 'O que é o "Hot Reload" no Flutter?',
+      'opcoes': {
+        'Reiniciar o aplicativo': 0,
+        'Atualizar o estado do aplicativo sem perder os dados': 1,
+        'Instalar dependências': 0,
+        'Criar um novo projeto': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito8.jpg',
+      'texto': 'Qual widget é usado para criar um botão em Flutter?',
+      'opcoes': {
+        'Button': 0,
+        'RaisedButton': 0,
+        'FlatButton': 0,
+        'ElevatedButton': 1,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito9.jpg',
+      'texto':
+          'Qual biblioteca é comumente usada para gerenciar estados em aplicativos Flutter?',
+      'opcoes': {
+        'Provider': 1,
+        'Axios': 0,
+        'Redux': 0,
+        'Firebase': 0,
+      },
+    },
+    {
+      'asset': 'lib/assets/gatito10.jpg',
+      'texto':
+          'Qual é a função principal de um arquivo `pubspec.yaml` em um projeto Flutter?',
+      'opcoes': {
+        'Definir a estrutura do aplicativo': 0,
+        'Gerenciar dependências e configurações do projeto': 1,
+        'Configurar a interface do usuário': 0,
+        'Especificar rotas do aplicativo': 0,
+      },
+    },
+  ];
+
+  void responder(String opcaoSelecionada) {
+    // Adiciona ou atualiza a pontuação da opção selecionada
+    pontuacoes[opcaoSelecionada] =
+        perguntas[indicePergunta]['opcoes'][opcaoSelecionada];
+
+    if (indicePergunta + 1 < perguntas.length) {
+      setState(() {
+        indicePergunta++;
+      });
+    } else {
+      // Mostra o resultado final
+      mostrarResultados();
+    }
+  }
+
+  void mostrarResultados() {
+    String resultado = 'Resultados:\n';
+    int pontuacaoFinal = 0;
+    pontuacoes.forEach((opcao, score) {
+      resultado += '$opcao: ${score == 1 ? "Correto" : "Incorreto"}\n';
+      pontuacaoFinal += score;
+    });
+    resultado += '\nPontuação final: $pontuacaoFinal/${perguntas.length}';
+
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Resultado'),
+        content: Text(resultado),
+        actions: [
+          TextButton(
+            child: Text('Fechar'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+              setState(() {
+                indicePergunta = 0; // Reiniciar o quiz
+                pontuacoes.clear(); // Limpar as pontuações
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Quiz')),
+      body: SingleChildScrollView(
+          // Permite rolagem do conteúdo
+          child: Center(
+        child: Column(
+          children: [
+            // Usando a URL para a imagem
+            Image.asset(
+              perguntas[indicePergunta]['asset'] as String,
+              fit: BoxFit.fitHeight,
+              width: MediaQuery.of(context).size.width *
+                  0.9, // Ajusta a largura da imagem
+              height: MediaQuery.of(context).size.height *
+                  0.4, // Ajusta a altura da imagem
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                perguntas[indicePergunta]['texto'],
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center, // Ajusta o alinhamento do texto
+              ),
+            ),
+            ...perguntas[indicePergunta]['opcoes'].keys.map((opcao) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ElevatedButton(
+                  onPressed: () => responder(opcao),
+                  child: Text(opcao),
+                ),
+              );
+            }).toList(),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/tela_inicio.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/quiz.dart';
+
+class TelaInicial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tela Inicial'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Título ou mensagem
+            Text(
+              'Bem-vindo ao Quiz!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            // Botão para iniciar o quiz
+            ElevatedButton(
+              onPressed: () {
+                // Navega para a tela do quiz
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PaginaQuiz()), // Navega para a tela PaginaQuiz
+                );
+              },
+              child: Text('Iniciar Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
