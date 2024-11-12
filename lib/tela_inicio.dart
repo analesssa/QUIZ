e95a577151f0e6fd251c181b034c0d1ac3104a15
ALUNO <ALUNO@senaiescola.fsa>
2024-11-12 11:19:45 -0300

    inserir nome

--- lib/tela_inicio.dart
+++ lib/tela_inicio.dart
@@ -1,7 +1,15 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/quiz.dart';
 
-class TelaInicial extends StatelessWidget {
+class TelaInicial extends StatefulWidget {
+  @override
+  _TelaInicialState createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  final TextEditingController _nomeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,18 +22,39 @@ class TelaInicial extends StatelessWidget {
           children: [
             // Título ou mensagem
             Text(
-              'Comece sua jornada aqui! ',
+              'Comece sua jornada aqui!',
               style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            // Campo de texto para o nome
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _nomeController,
+                decoration: InputDecoration(
+                  labelText: 'Digite o seu nome aqui!',
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ),
             SizedBox(height: 20),
             // Botão para iniciar o quiz
             ElevatedButton(
               onPressed: () {
-                // Navega para a tela do quiz
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PaginaQuiz()), // Navega para a tela PaginaQuiz
-                );
+                final nome = _nomeController.text;
+                if (nome.isNotEmpty) {
+                  // Navega para a tela do quiz e passa o nome do usuário
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PaginaQuiz()),
+                  );
+                } else {
+                  // Exibe um alerta caso o nome não tenha sido preenchido
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Por favor, insira seu nome!')),
+                  );
+                }
               },
               child: Text('Iniciar Quiz'),
             ),
